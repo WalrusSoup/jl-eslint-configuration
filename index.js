9862da587a7bcc215fc82f09beded12d5fef1e6f
JL <jay.lindsley@gmail.com>
2022-01-26 11:20:35 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+module.exports = {
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        htmlSingleAttributePerLine: 'off',
+        'array-bracket-newline': 'off',
+        'array-element-newline': 'off',
+        'object-property-newline': [
+            'error',
+            {
+                allowAllPropertiesOnSameLine: true,
+            },
+        ],
+        'vue/first-attribute-linebreak': [
+            'error',
+            {
+                singleline: 'beside',
+                multiline: 'beside',
+            },
+        ],
+        'vue/html-closing-bracket-newline': [
+            'error',
+            {
+                singleline: 'never',
+                multiline: 'never',
+            },
+        ],
+        'vue/max-attributes-per-line': [
+            'error',
+            {
+                singleline: {
+                    max: 10,
+                },
+                multiline: {
+                    max: 10,
+                },
+            },
+        ]
+    }
+};
